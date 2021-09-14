9c7ba9e9cb10bb9af57ecaeffa1fa15cf0b86ac0
heyhewmike <75391794+heyhewmike@users.noreply.github.com>
2021-09-13 23:10:29 -0400

    Update update-ip.sh

--- update-ip.sh
+++ update-ip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Copyright © 2021 Michael Kudrak <mike@heyhew.net>
 ip=$(dig example.com | grep -A 2 'ANSWER SECTION' | grep IN | cut -f5)
-echo "allow "${ip}";" > /etc/nginx/access/home.conf 
-
+echo "allow "${ip}";" > /etc/nginx/path/to/file.conf
